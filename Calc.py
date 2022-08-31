69bd7979c3a3a35ab2b7a82052050b0049f5d5e8
LocatelliBryann <bryannlocatelli@gmail.com>
2022-08-30 22:02:30 -0300

    ValidaçãoMask-ajustes

--- Calc.py
+++ Calc.py
@@ -1,46 +1,53 @@
 # CALCULADORA DE IP
 # Desenvolvida por Bryann Locatelli - 3INFO1
 
-import emoji
+import emoji #Importando biblioteca de emojis
+from math import ceil #Importando biblioteca de matemática para arredondamento
+
 
 barrinha = "\033[1;33m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\033[1;33m"
-print(barrinha)
-print("              \033[1;31mCALCULADORA \033[1;32mDE \033[1;94mIP\033[0;0m")
-print(emoji.emojize(":exploding_head:", language='alias'),"Desenvolvida por Bryann Locatelli - 3INFO1",emoji.emojize(":exploding_head:", language='alias'))
+
+print(barrinha)                                                                                                                                              #
+print("              \033[1;31mCALCULADORA \033[1;32mDE \033[1;94mIP\033[0;0m")                                                                              #Cabeçalho
+print(emoji.emojize(":exploding_head:", language='alias'),"Desenvolvida por Bryann Locatelli - 3INFO1",emoji.emojize(":exploding_head:", language='alias'))  #
+print(barrinha) 
 
 v = 0
 ip = []
 while v == 0:
-    ip_classe = str(input("\033[1;32mInforme qual a classe da rede desejada \033[1;96m(A , B , C) \033[1;32m=>\033[1;32m "))
+    ip_classe = str(input("\033[1;32mInforme qual a classe da rede desejada \033[1;96m(\033[1;35mA , B , C\033[1;96m) \033[1;32m=>\033[0;0m"))
 
     if ip_classe == "A" or ip_classe == "a" or ip_classe == "B" or ip_classe == "b" or ip_classe == "C" or ip_classe == "c":   #Verificação se a classe de IP informada é válida (A, B ou C)
-        mascara = int(input("\033[1;32mInforme a máscara de rede em bits: \033[1;96mExemplo:\033[1;35m26\033[0;0m => "))
-        ip = [int(i) for i in input('Digite o endereço de IPV4 (separe por pontos): ').split('.')]
+        
+        mascara = int(input("\033[1;32mInforme a máscara de rede em bits: \033[1;96mExemplo:\033[1;35m26 \033[1;32m=>\033[0;0m "))
+        if mascara <= 32:
+
+            ip = [int(i) for i in input('Digite o endereço de IPV4 (separe por pontos): ').split('.')]
 
 
-        if ip_classe == 'A' or ip_classe == 'a':
-            bits_masc = ip[1:4]
-            ip = [ip[0]]
-            bits_ip = 8
-            v = 1
-        elif ip_classe == 'B' or ip_classe == 'b':
-            bits_masc = ip[2:4]
-            ip = ip[0:2]
-            bits_ip = 16
-            v = 1
-        elif ip_classe == 'C' or ip_classe == 'c':
-            bits_masc = [ip[-1]]
-            ip = ip[0:3]
-            bits_ip = 24
-            v = 1 
+            if ip_classe == 'A' or ip_classe == 'a':
+                bits_masc = ip[1:4]
+                ip = [ip[0]]
+                bits_ip = 8
+                v = 1
+            elif ip_classe == 'B' or ip_classe == 'b':
+                bits_masc = ip[2:4]
+                ip = ip[0:2]
+                bits_ip = 16
+                v = 1
+            elif ip_classe == 'C' or ip_classe == 'c':
+                bits_masc = [ip[-1]]
+                ip = ip[0:3]
+                bits_ip = 24
+                v = 1 
+
+        else:
+            print('\033[1;31mMÁSCARA INVÁLIDA\033[0;0m')       
     else:
         print('\033[1;31mCLASSE DE IP INVÁLIDA\033[0;0m')
 
 
 
-
-
-from math import ceil
 
 bits_mascara = mascara - bits_ip
 octetos = ceil(bits_mascara/8)
