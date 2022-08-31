2d5329b2d76ef0258faf1a82f01e8510a356e2e4
LocatelliBryann <bryannlocatelli@gmail.com>
2022-08-31 20:57:06 -0300

    ValidaçõesSleep-Colors

--- Calc.py
+++ Calc.py
@@ -3,6 +3,7 @@
 
 import emoji #Importando biblioteca de emojis
 from math import ceil #Importando biblioteca de matemática para arredondamento
+import time #Importando biblioteca de controle de tempo
 
 
 barrinha = "\033[1;33m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\033[1;33m"
@@ -16,28 +17,35 @@ v = 0
 ip = []
 while v == 0:
     ip_classe = str(input("\033[1;32mInforme qual a classe da rede desejada \033[1;96m(\033[1;35mA , B , C\033[1;96m) \033[1;32m=>\033[0;0m"))
+    print(f"Aguarde...")
+    time.sleep(1.5)
 
     if ip_classe == "A" or ip_classe == "a" or ip_classe == "B" or ip_classe == "b" or ip_classe == "C" or ip_classe == "c":                                  #Verificação se a classe de IP informada é válida (A, B ou C)
-        
-        mascara = int(input("\033[1;32mInforme a máscara de rede em bits: \033[1;96mExemplo:\033[1;35m26 \033[1;32m=>\033[0;0m "))
-        if mascara <= 32:                                                                                                                                      #Verificação se a máscara de IP informada é válida
+        print(emoji.emojize(":check_mark_button:", language='alias'),"\033[1;32mCLASSE DE IP VÁLIDA\033[0;0m")
 
-            ip = [int(i) for i in input('Digite o endereço de IPV4 (separe por pontos): ').split('.')]
+        mask = int(input("\033[1;32mInforme a máscara de rede em bits: \033[1;96mExemplo:\033[1;35m26 \033[1;32m=>\033[0;0m "))
+        print(f"Aguarde...")
+        time.sleep(1.5)
+
+        if mask <= 32:                                                                                                                                      #Verificação se a máscara de IP informada é válida
+            print(emoji.emojize(":check_mark_button:", language='alias'),"\033[1;32mMÁSCARA DE IP VÁLIDA\033[0;0m")
+
+            ip = [int(i) for i in input('\033[1;32mInforme o endereço de IP \033[1;96m(\033[1;35mSeparado por pontos\033[1;96m) \033[1;32m=>\033[0;0m ').split('.')]    #Recebe o input e já separa o IP pelo "."
 
             if ip_classe == 'A' or ip_classe == 'a':
-                bits_masc = ip[1:4]
+                mask_bits = ip[1:4]
                 ip = [ip[0]]
-                bits_ip = 8
+                ip_bits = 8
                 v = 1
             elif ip_classe == 'B' or ip_classe == 'b':
-                bits_masc = ip[2:4]
+                mask_bits = ip[2:4]
                 ip = ip[0:2]
-                bits_ip = 16
+                ip_bits = 16
                 v = 1
             elif ip_classe == 'C' or ip_classe == 'c':
-                bits_masc = [ip[-1]]
+                mask_bits = [ip[-1]]
                 ip = ip[0:3]
-                bits_ip = 24
+                ip_bits = 24
                 v = 1 
 
         else:
@@ -45,9 +53,9 @@ while v == 0:
     else:
         print(emoji.emojize(":cross_mark:", language='alias'),'\033[1;31mCLASSE DE IP INVÁLIDA\033[0;0m',emoji.emojize(":cross_mark:", language='alias'))
 
+print(mask_bits)
 
-
-bits_mascara = mascara - bits_ip
+bits_mascara = mask - ip_bits
 octetos = ceil(bits_mascara/8)
 lista_mascara = [[] for i in range(octetos)]
 
